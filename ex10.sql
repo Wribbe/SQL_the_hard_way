732cbdb8244b33aa9a5f23d18c801b8bdbe50d24
Stefan Eng <eng_steff@hotmail.com>
2018-09-06 18:25:14 +0200

    Basic ex10 done.

---
+++ ex10.sql
@@ -0,0 +1,77 @@
+CREATE TABLE person (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pet (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT,
+  age INTEGER,
+  dead INTEGER
+);
+
+CREATE TABLE person_pet (
+  person_id INTEGER,
+  pet_id INTEGER
+);
+
+CREATE TABLE car (
+  id INTIGER PRIMARY KEY,
+  year INTEGER,
+  model TEXT,
+  manifacturer TEXT,
+  color TEXT
+);
+
+CREATE TABLE person_car (
+  person_id INTEGER,
+  car_id INTEGER
+);
+
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (0, 'Zed', 'Shaw', 37);
+
+INSERT INTO pet (id, name, breed, age, dead)
+  VALUES (0, 'Fluffy', 'Unicorn', 1000, 0);
+
+INSERT INTO pet VALUES(1, 'Gigantor', 'Robot', 1, 1);
+
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (1, 'Stefan', 'Eng', 31);
+
+INSERT INTO pet (id, name, breed, age, dead)
+  VALUES (2, 'Billy', 'Cat', 13, 1);
+
+INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
+INSERT INTO person_pet VALUES (0, 1);
+
+INSERT INTO person_pet (person_id, pet_id) VALUES (1,2);
+
+INSERT INTO person_pet (person_id, pet_id) VALUES (1,1);
+
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (2, 'Ari', 'Mari', 15);
+DELETE FROM pet WHERE dead = 1 AND name = 'Gigantor';
+INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 0);
+
+SELECT * FROM pet;
+
+UPDATE pet SET name = "Zed's Pet" WHERE id IN (
+  SELECT pet.id
+  FROM pet, person_pet, person
+  WHERE
+    person.id = person_pet.person_id AND
+    pet.id = person_pet.pet_id AND
+    person.first_name = "Zed"
+);
+
+SELECT * FROM pet;
+
+
+/*
+  Drills:
+  -------
+*/
