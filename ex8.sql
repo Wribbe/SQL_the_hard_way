85d3c889de87f5cbec6864ee36daabbc3daec919
Stefan Eng <eng_steff@hotmail.com>
2018-09-06 09:13:56 +0200

    Complete drills for ex8.

--- ex8.sql
+++ ex8.sql
@@ -109,21 +109,65 @@ SELECT * FROM pet;
 INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 0);
 SELECT * FROM pet;
 
-DELETE FROM pet WHERE id IN (
-  SELECT pet.id
-  FROM pet, person_pet, person
-  WHERE
-  person.id = person_pet.person_id AND
-  pet.id = person_pet.pet_id AND
-  person.first_name = 'Zed'
-);
+--DELETE FROM pet WHERE id IN (
+--  SELECT pet.id
+--  FROM pet, person_pet, person
+--  WHERE
+--  person.id = person_pet.person_id AND
+--  pet.id = person_pet.pet_id AND
+--  person.first_name = 'Zed'
+--);
+--
+--SELECT * FROM pet;
+--SELECT * FROM person_pet;
+--
+--DELETE FROM person_pet
+--  WHERE pet_id NOT IN (
+--    SELECT id FROM pet
+--  );
+--
+--SELECT * FROM person_pet;
+--
+--DELETE FROM pet WHERE id IN (
+--  SELECT pet.id FROM pet, person_pet, person
+--  WHERE
+--  person.id = person_pet.person_id AND
+--  pet.id = person_pet.pet_id AND
+--  person.first_name = 'Stefan' AND
+--  pet.dead = 1
+--);
+--
+--SELECT * FROM pet;
+--SELECT * FROM person_pet;
+--
+--DELETE FROM person_pet
+--  WHERE pet_id NOT IN (
+--    SELECT id FROM pet
+--  );
+--
+--SELECT * FROM person_pet;
 
-SELECT * FROM pet;
+SELECT * FROM person;
+SELECT * FROM person_pet;
+
+-- Remove persons that own a dead pet.
+DELETE FROM person
+  WHERE person.id IN (
+    SELECT person.id FROM person, pet, person_pet
+      WHERE
+      person.id = person_pet.person_id AND
+      pet.id = person_pet.pet_id AND
+      pet.dead = 1
+  );
+
+SELECT * FROM person;
+
+-- Remove dead pets from person_pet table.
 SELECT * FROM person_pet;
 
 DELETE FROM person_pet
-  WHERE pet_id NOT IN (
-    SELECT id FROM pet
+  WHERE person_pet.pet_id IN (
+    SELECT pet.id FROM pet WHERE pet.dead = 1
   );
 
 SELECT * FROM person_pet;
@@ -131,4 +175,6 @@ SELECT * FROM person_pet;
 /*
   Drills:
   -------
+   1.) Delete dead pets that you own.
+   2.) Delete people with dead pets.
 */
